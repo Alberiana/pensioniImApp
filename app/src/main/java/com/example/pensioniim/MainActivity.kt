79cea44e86f288f19442488327c564928f189ff4
Alberiana Tofaj <alberianatofaj@gmail.com>
2024-06-23 15:58:10 +0200

    Add initial main and face liveness screens with navigation

--- app/src/main/java/com/example/pensioniim/MainActivity.kt
+++ app/src/main/java/com/example/pensioniim/MainActivity.kt
@@ -8,15 +8,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -24,11 +31,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -68,6 +82,12 @@ class MainActivity : ComponentActivity() {
                     Log.d("MainActivity", "Navigating to ResultScreen with ViewModel: $viewModel")
                     ResultScreen(viewModel = viewModel, onBack = { navController.popBackStack() })
                 }
+                composable("IDCardScreen") {
+                    IDCardScreen(navController)
+                }
+                composable("ContinueVerificationScreen") {
+                    ContinueVerificationScreen(navController)
+                }
             }
         }
     }
@@ -84,22 +104,38 @@ class MainActivity : ComponentActivity() {
         }
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcome to Your App!")
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(Modifier.size(280.dp))
+            Image(
+                painter = painterResource(id = R.drawable.pensioni),
+                contentDescription = null
+            )
+            Text(
+                text = "Mirësevini në aplikacionin ku mund te verfikoheni per pensionin tuaj!",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(20.dp),
+                style = TextStyle(
+                    fontSize = 38.sp,
+                    fontWeight = FontWeight.SemiBold,
+                )
+            )
+            Spacer(Modifier.weight(1f))
+            Button(
+                onClick = {
+                    navController.navigate("IDCardScreen")
 
-            Button(onClick = {
-                if (checkCameraPermission()) {
-                    navController.navigate("faceLivenessScreen")
-                } else {
-                    cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
-                }
-            }) {
-                Text("Start Face Liveness Detection")
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .width(300.dp)
+                    .height(60.dp)
+            )
+            {
+                Text("Vazhdo")
             }
-
         }
     }
 
@@ -152,6 +188,109 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    fun IDCardScreen(navController: NavHostController) {
+        var text by remember {
+            mutableStateOf("")
+        }
+        var isError by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(Modifier.size(300.dp))
+            Text(
+                text = "Ju lutem jepni numrin personal të letërnjoftimit: ",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(20.dp),
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold,
+                )
+            )
+            OutlinedTextField(
+                value = text,
+                shape = RoundedCornerShape(12.dp),
+                onValueChange = {
+                    if (it.length <= 10 && it.all { char -> char.isDigit() }) {
+                        text = it
+                        isError = it.length != 10
+                    }
+                },
+                label = {
+                    Text(
+                        text = if (isError) "Numri i ID duhet të jetë saktësisht 10 shifra" else "ID e letërnjoftimit",
+                        style = TextStyle(color = if (isError) Color.Red else Color.Gray)
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(Modifier.weight(1f))
+            Button(
+                onClick = {
+                    navController.navigate("ContinueVerificationScreen")
+
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .width(300.dp)
+                    .height(60.dp)
+            ){
+                Text("Vazhdo")
+            }
+        }
+    }
+    @Composable
+    fun ContinueVerificationScreen(navController: NavHostController) {
+        var text by remember {
+            mutableStateOf("")
+        }
+        var isError by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(Modifier.size(300.dp))
+            Text(
+                text = "Mirëseerdhe Jana!",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(20.dp),
+                style = TextStyle(
+                    fontSize = 38.sp,
+                    fontWeight = FontWeight.SemiBold,
+                )
+            )
+            Spacer(Modifier.weight(1f))
+            Button(
+                onClick = {
+                    if (checkCameraPermission()) {
+                        navController.navigate("faceLivenessScreen")
+                    } else {
+                        cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+                    }
+
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .width(300.dp)
+                    .height(60.dp)
+            ) {
+                Text("Vazhdo")
+            }
+        }
+    }
+
     @Preview
     @Composable
     fun previewMainActivity() {
